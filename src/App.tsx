10ebc35f2d8fcdd45cb8e42162f664039184235b
ynawhocodes <ynawhocodes@gmail.com>
2025-08-29 11:40:18 +0900

    feat: 스티커가 가장자리에 있을 때 스크롤 방지

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import StickerPeel from './components/StickerPeel';
 function App() {
   return (
     <div className='w-full h-full bg-black overflow-hidden'>
-      <StickerPeel imageSrc={sticker} width={300} rotate={15} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 50, y: 100 }} label='양나원' />
+      <StickerPeel imageSrc={sticker} width={300} rotate={15} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.05} initialPosition={{ x: 50, y: 100 }} label='양나원' />
       <StickerPeel imageSrc={epSticker} width={300} rotate={5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 150, y: 350 }} label='Everyday Practice' />
     </div>
   );
