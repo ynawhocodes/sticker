2c0580516a81c18548c6f0ac7b977c5c7c673560
ynawhocodes <ynawhocodes@gmail.com>
2025-09-05 18:07:13 +0900

    mod: 라벨 변경

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <StickerPeel imageSrc={ynwSticker} width={300} rotate={15} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.05} initialPosition={{ x: 50, y: 100 }} label='양나원' />
       <StickerPeel imageSrc={epSticker} width={300} rotate={5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 150, y: 350 }} label='Everyday Practice' />
       <StickerPeel imageSrc={suiSticker} width={300} rotate={-5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 650, y: 200 }} label='정수이' />
-      <StickerPeel imageSrc={hySticker} width={300} rotate={-5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 850, y: 500 }} label='정수이' />
+      <StickerPeel imageSrc={hySticker} width={300} rotate={-5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 850, y: 500 }} label='양현호' />
     </div>
   );
 }
