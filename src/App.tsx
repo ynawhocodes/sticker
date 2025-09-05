18e715cf0d58ecb8ef12976b6c255b96360bcfa1
ynawhocodes <ynawhocodes@gmail.com>
2025-09-05 18:06:04 +0900

    feat: 현호씨 스티커 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
-import sticker from './assets/sticker.png';
+import ynwSticker from './assets/ynw-sticker.png';
 import epSticker from './assets/ep-sticker.png';
 import StickerPeel from './components/StickerPeel';
 import suiSticker from './assets/sui-sticker.png';
+import hySticker from './assets/hy-sticker.png';
 
 function App() {
   return (
     <div className='w-full h-full bg-black overflow-hidden'>
-      <StickerPeel imageSrc={sticker} width={300} rotate={15} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.05} initialPosition={{ x: 50, y: 100 }} label='양나원' />
+      <StickerPeel imageSrc={ynwSticker} width={300} rotate={15} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.05} initialPosition={{ x: 50, y: 100 }} label='양나원' />
       <StickerPeel imageSrc={epSticker} width={300} rotate={5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 150, y: 350 }} label='Everyday Practice' />
       <StickerPeel imageSrc={suiSticker} width={300} rotate={-5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 650, y: 200 }} label='정수이' />
+      <StickerPeel imageSrc={hySticker} width={300} rotate={-5} peelBackHoverPct={30} peelBackActivePct={70} shadowIntensity={0.6} lightingIntensity={0.1} initialPosition={{ x: 850, y: 500 }} label='정수이' />
     </div>
   );
 }
